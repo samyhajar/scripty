43e18df0fc1fde0bc4d725efa514e81dacc7f640
samyhajar <samy.hajar@gmail.com>
2024-10-27 03:25:55 +0100

    added the nice unfold way for each apps

--- django_projects/kiva/kiva/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("__reload__/", include("django_browser_reload.urls")),
-    path('', include(f"{settings.HOME_APP}.urls")),
-]
